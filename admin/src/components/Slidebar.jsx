0046e4253daec2d0eff816958a191e2873c3abee
hungnbkhp08 <hungdepzaihehe@gmail.com>
2025-11-03 09:32:47 +0700

    fe update skide bar

--- admin/src/components/Slidebar.jsx
+++ admin/src/components/Slidebar.jsx
@@ -17,13 +17,9 @@ const Slidebar = () => {
           <img src={assets.appointment_icon} alt="" />
           <p className='hidden md:block'>Lịch khám</p>
         </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/add-doctor'}>
-          <img src={assets.add_icon} alt="" />
-          <p className='hidden md:block'>Thêm bác sỹ</p>
-        </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-list'}>
-          <img src={assets.people_icon} alt="" />
-          <p className='hidden md:block'>Danh sách bác sỹ</p>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/wallet-management'}>
+          <img src={assets.wallet} alt="" />
+          <p className='hidden md:block'>Quản lý ví</p>
         </NavLink>
         <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/patient-records'}>
           <img src={assets.patient} alt="" />
@@ -33,9 +29,13 @@ const Slidebar = () => {
           <img src={assets.star} alt="" />
           <p className='hidden md:block'>Quản lý đánh giá</p>
         </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/wallet-management'}>
-          <img src={assets.wallet} alt="" />
-          <p className='hidden md:block'>Quản lý ví</p>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-list'}>
+          <img src={assets.people_icon} alt="" />
+          <p className='hidden md:block'>Danh sách bác sỹ</p>
+        </NavLink>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/add-doctor'}>
+          <img src={assets.add_icon} alt="" />
+          <p className='hidden md:block'>Thêm bác sỹ</p>
         </NavLink>
       </ul>}
       {dToken && <ul className='text-[#515151] mt-5'>
@@ -47,21 +47,21 @@ const Slidebar = () => {
           <img src={assets.appointment_icon} alt="" />
           <p className='hidden md:block'>Lịch khám</p>
         </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-profile'}>
-          <img src={assets.people_icon} alt="" />
-          <p className='hidden md:block'>Thông tin</p>
-        </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-reviews'}>
-          <img src={assets.star} alt="" />
-          <p className='hidden md:block'>Đánh giá</p>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/chat'}>
+          <img src={assets.chat} alt="" />
+          <p className='hidden md:block'>Tin nhắn</p>
         </NavLink>
         <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-wallet'}>
           <img src={assets.wallet} alt="" />
           <p className='hidden md:block'>Quản lý ví</p>
         </NavLink>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/chat'}>
-          <img src={assets.chat} alt="" />
-          <p className='hidden md:block'>Tin nhắn</p>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-reviews'}>
+          <img src={assets.star} alt="" />
+          <p className='hidden md:block'>Đánh giá</p>
+        </NavLink>
+        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]' : ''}`} to={'/doctor-profile'}>
+          <img src={assets.people_icon} alt="" />
+          <p className='hidden md:block'>Thông tin</p>
         </NavLink>
       </ul>}
 
